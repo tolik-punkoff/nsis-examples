3f5b43f87fc31deca6c2e3cb11a5a0999d032e67
Tolik Punkoff <tolikgit@inbox.lv>
2022-05-12 02:50:02 +0300

    Create different.asm

---
+++ testapp-src/different.asm
@@ -0,0 +1,23 @@
+.386
+.MODEL FLAT
+
+	extrn	ExitProcess:proc
+	extrn	MessageBoxA:proc
+	
+.DATA
+
+	MSG_TITLE	DB	'TestApp',0
+	MSG_MESSAGE	DB	'I am different!',0
+	MB_INFORMATION	DD 40h
+
+.CODE
+Start:
+	push	MB_INFORMATION		;Message box style (Icon - Information)
+	push	offset MSG_TITLE	;Message box title
+	push	offset MSG_MESSAGE	;Message box text
+	push	0					;hwndOwner
+	call	MessageBoxA
+	
+	push	0
+	call	ExitProcess
+end	Start
